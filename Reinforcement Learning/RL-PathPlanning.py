bbf6489117fc32085f0448529886c73bd1b964a2
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2016-06-06 12:00:56 -0300

    Crawler fixed

--- Reinforcement Learning/RL-PathPlanning.py
+++ Reinforcement Learning/RL-PathPlanning.py
@@ -62,7 +62,7 @@ class Draw(Thread):
 class ReinforcementLearning(object):
     def __init__(self):
         self.discount = 0.9
-        self.reward = -0.008
+        self.reward = -0.08
 
         self.rl_body_linVelocity = 0.0;
         self.rl_generation = 0
@@ -89,7 +89,7 @@ class ReinforcementLearning(object):
             self.body_last_pos = crawlerPos.x
 
 
-        i,j = int(joint_angles[0]/6.0),int(joint_angles[1]/6.0)
+        i,j = int(joint_angles[0]/6.0 -1),int(joint_angles[1]/6.0 -1)
         if(i > len(self.matrix_states)-1): i = len(self.matrix_states)-1
         if(j > len(self.matrix_states)-1): j = len(self.matrix_states)-1
         if(i < 0): i = 0
@@ -173,16 +173,15 @@ class ReinforcementLearning(object):
                 fut_state.extend([i - 1,j - 1])
                 possible_actions.append("22")
 
-            if(fut_state[0] < 0): fut_state[0] = 0
-            if(fut_state[1] < 0): fut_state[1] = 0
-            if(fut_state[0] > max_ind): fut_state[0] = max_ind
-            if(fut_state[1] > max_ind): fut_state[1] = max_ind
+            if(fut_state[0] < 0 or fut_state[1] < 0 or fut_state[0] > max_ind or fut_state[1] > max_ind): 
+                possible_state_values.append(-100000)
+            else:
+                act_state = []
+                act_state.append(i)
+                act_state.append(j)
+                possible_state_values.append(self.calculateLocalQStates(fut_state, act_state))
 
-            act_state = []
-            act_state.append(i)
-            act_state.append(j)
-            possible_state_values.append(self.calculateLocalQStates(fut_state, act_state))
-
+        print(possible_actions)
         max_value = -99999
         max_action = ""
         for action in range(0,8):
@@ -196,7 +195,7 @@ class ReinforcementLearning(object):
         return max_action
 
     def calculateLocalQStates(self, fut_state, act_state):
-        return (1 - 1/self.matrix_visits[act_state[0]][act_state[1]])*self.matrix_states[act_state[0]][act_state[1]] + 1/self.matrix_visits[act_state[0]][act_state[1]]*(self.reward + self.rl_body_linVelocity/5 + self.discount*self.adjustStateValue(fut_state[0],fut_state[1]))
+        return (1 - 1/self.matrix_visits[act_state[0]][act_state[1]])*self.matrix_states[act_state[0]][act_state[1]] + 1/self.matrix_visits[act_state[0]][act_state[1]]*(self.reward + self.rl_body_linVelocity + self.discount*self.adjustStateValue(fut_state[0],fut_state[1]))
 
     def GetGeneration(self):
         return self.rl_generation
@@ -224,11 +223,11 @@ class Physics(Thread):
 
         self.__create_bodies()
 
-    def __create_dynamic_car(self,xpos,ypos,velocity):
+    def __create_dynamic_car(self,xpos,ypos):
         body= self.world.CreateDynamicBody(position=(xpos/self.pygame_box2d_ratio,ypos/self.pygame_box2d_ratio))
-        box=body.CreatePolygonFixture(box=(self.car_x_size/2.0,self.car_y_size/2.0), density=0.05, friction=100000)
-        body.linearVelocity=(0,velocity)
-        body.angularDamping=0.6
+        box=body.CreatePolygonFixture(box=(self.car_x_size/2.0,self.car_y_size/2.0), density=0.005, friction=100000)
+        body.linearVelocity=(0,0)
+        body.angularDamping=0.0
         return body
 
     def __create_joint(self, body1, body2, ptJoint, conCollision):
@@ -236,10 +235,10 @@ class Physics(Thread):
         jointDef.Initialize(body1, body2, ptJoint)
         jointDef.collideConnected = conCollision
         jointDef.lowerAngle = 00
-        jointDef.upperAngle = 60
+        jointDef.upperAngle = math.radians(60)
         jointDef.enableLimit = True
         jointDef.maxMotorTorque = 50000
-        jointDef.motorToque = 50000
+        #jointDef.motorToque = 50000
         jointDef.motorSpeed = 0
         jointDef.enableMotor = True
 
@@ -248,24 +247,26 @@ class Physics(Thread):
 
     def __create_bodies(self):
         #Create the crawler arms
-        self.bodylist.update({"arm1":self.__create_dynamic_car(270,130,0)})
-        self.bodylist.update({'arm2':self.__create_dynamic_car(190,130,0)})
+        self.bodylist.update({"arm1":self.__create_dynamic_car(270,130)})
+        self.bodylist.update({'arm2':self.__create_dynamic_car(190,130)})
+        
+        
 
         #Create the crawler body
         body = self.world.CreateDynamicBody(position=(5,3.5))
         box = body.CreatePolygonFixture(box=(2.5,2.5), density=0.07, friction=1.5)
         self.bodylist.update({'body':body})
 
+        #Joint connceting arms and the crawler body
+        ptJointB = self.bodylist['body'].worldCenter + b2Vec2(2.5,2.5) 
+
+        self.jointlist.append(self.__create_joint(bodylist['arm2'],bodylist['body'], ptJointB,True))
+
         #Joint connecting both arms
         ptJoint = self.bodylist['arm1'].worldCenter + self.bodylist['arm2'].worldCenter
         ptJoint = ptJoint/2
 
         self.jointlist.append(self.__create_joint(bodylist['arm1'],bodylist['arm2'], ptJoint,False))
-
-        #Joint connceting arms and the crawler body
-        ptJointB = self.bodylist['body'].worldCenter + b2Vec2(2.5,2.5) 
-
-        self.jointlist.append(self.__create_joint(bodylist['arm2'],bodylist['body'], ptJointB,True))
 
         #Create floor
         body = self.world.CreateStaticBody(position=(0,0),shapes=polygonShape(box=(1000,1)))
@@ -281,10 +282,11 @@ class Physics(Thread):
         for joint in range(len(self.jointlist)):
             #print(joint)
             actJoint = self.jointlist[joint]
-            #print(math.degrees(actJoint.angle))
             if(self.actions[joint] == "1"):
                 if(self.reaching_angle[joint] is not True): 
                     self.angle_to_reach[joint] = math.degrees(self.jointlist[joint].angle) + 6
+                    actJoint.lowerAngle = self.jointlist[joint].angle - math.radians(2)
+                    actJoint.upperAngle = math.radians(self.angle_to_reach[joint]+2)
                     if(self.angle_to_reach[joint] > 60.0): self.angle_to_reach[joint] = 60.0
                     self.reaching_angle[joint] = True
                 
@@ -296,6 +298,8 @@ class Physics(Thread):
             if(self.actions[joint] == "2"):
                 if(self.reaching_angle[joint] is not True): 
                     self.angle_to_reach[joint] = math.degrees(self.jointlist[joint].angle) - 6
+                    actJoint.upperAngle = self.jointlist[joint].angle + math.radians(2)
+                    actJoint.lowerAngle = math.radians(self.angle_to_reach[joint]-2)
                     if(self.angle_to_reach[joint] < 0.0): self.angle_to_reach[joint] = 0.0
                     self.reaching_angle[joint] = True
 
@@ -311,7 +315,7 @@ class Physics(Thread):
         while running:
             self.ev = self.pygame.event.poll()
             keys = self.pygame.key.get_pressed()
-            time.sleep(1/5*self.timeStep)
+            time.sleep(1/10*self.timeStep)
             self.__physicsLoop()
         return
 
